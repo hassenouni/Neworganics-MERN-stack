06e1abc25f479a7aa1072c2fda7b024b0778cc49
Hassen <ouni.hassen.ouni@gmail.com>
2020-11-04 17:10:01 +0100

    modify Herro-Banner

--- client/src/components/Hero-banner.js
+++ client/src/components/Hero-banner.js
@@ -1,49 +1,89 @@
-import React from 'react'
-import {
-  isMobile
-} from "react-device-detect";
-import ButtonLinkCategoryShopPage from './Button-link-CategoryShop-page'
-import { Jumbotron, Container } from 'reactstrap';
+import React from "react";
+import { isMobile } from "react-device-detect";
+import ButtonLinkCategoryShopPage from "./Button-link-CategoryShop-page";
+import { Jumbotron, Container } from "reactstrap";
 
 const styles = {
   bannerCoverPc: {
     backgroundImage: 'url("/images/banner-cover-pc.jpg")',
-    backgroundSize: 'cover'
+    backgroundSize: "cover",
   },
   bannerCoverMobile: {
     backgroundImage: 'url("/images/banner-cover-mobile.jpg")',
-    backgroundSize: 'cover'
+    backgroundSize: "cover",
   },
   textBanner: {
-    textShadow: "3px 3px 3px grey",
-    textAlign: 'center',
-    color:'white'
+    textAlign: "center",
+    color: "white",
   },
   centerButtons: {
-    textAlign: 'center',
-    padding: '30px'
+    textAlign: "center",
+    padding: "30px",
   },
   titleH1Pc: {
-    fontSize: '80px'
+    fontSize: "46px",
+    color: "#252525",
+    lineHeight: "52px",
+    fontWeight: "700",
+    margin: "20px 0 15px 80px",
   },
   titleH1Mobile: {
-    fontSize: '60px'
-  }
+    fontSize: "60px",
+  },
 };
 
-const { bannerCoverPc, bannerCoverMobile, textBanner, centerButtons, titleH1Mobile, titleH1Pc } = styles
+const {
+  bannerCoverPc,
+  bannerCoverMobile,
+  textBanner,
+  centerButtons,
+  titleH1Mobile,
+  titleH1Pc,
+} = styles;
 
 const HeroBanner = () => (
-  <Jumbotron fluid style={isMobile? bannerCoverMobile : bannerCoverPc}>
-    <Container fluid style={{height: '300px'}}>
+  <Jumbotron fluid style={isMobile ? bannerCoverMobile : bannerCoverPc}>
+    <Container fluid style={{ height: "300px" }}>
       <div style={textBanner}>
-        <h3 className="display-3" style={isMobile ? titleH1Mobile : titleH1Pc}>Vegetable 100% Organic</h3>
-        <p>FRUIT FRESH</p>
-        <p>Free Pickup and Delivery Available</p>
+        <h2 className="display-3" style={isMobile ? titleH1Mobile : titleH1Pc}>
+          Vegetable
+          <br></br> 100% Organic
+        </h2>
+        <span
+          style={{
+            fontSize: "16px",
+            textTransform: "uppercase",
+            fontWeight: "700",
+            letterSpacing: "4px",
+            color: "#f8f9fa",
+            fontFamily: "Cairo, sans-serif",
+            margin: "20px 0 15px 80px",
+          }}
+        >
+          FRUIT FRESH
+        </span>
+        <p
+          style={{
+            fontSize: "18px",
+            fontWeight: "400",
+            lineHeight: "26px",
+            color: "#f8f9fa",
+            fontFamily: "Cairo, sans-serif",
+            margin: "20px 0 15px 80px",
+          }}
+        >
+          Free Pickup and Delivery Available
+        </p>
       </div>
       <div style={centerButtons}>
-        <ButtonLinkCategoryShopPage CategoryShop={'foods'} content='shop foods' />
-        <ButtonLinkCategoryShopPage CategoryShop={'cosmetics'} content='shop cosmetics' />
+        <ButtonLinkCategoryShopPage
+          CategoryShop={"foods"}
+          content="shop foods"
+        />
+        <ButtonLinkCategoryShopPage
+          CategoryShop={"cosmetics"}
+          content="shop cosmetics"
+        />
       </div>
     </Container>
   </Jumbotron>
